60b2c966190d4835845962ec378c4a03f692dee4
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-10 20:58:21 +0000

    initial commit

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -31,37 +31,34 @@ export default function MainPage({ columns, students, allSubjects, recentlyUpdat
         <>
             <div className="min-h-screen bg-gray-100 text-gray-900 py-6">
                 <main className="mx-auto px-4 sm:px-6 lg:px-32 pt-4">
-                    <div className="flex justify-between">
+                    <div className="">
                         <h1 className="text-3xl font-bold flex tracking-wide pb-6">
                             <img src='/public/brand-logo.png' className='w-20 h-20 mr-3'/>
                             Student Management <br/> System
                         </h1>
-                        <button
-                            type='button'
-                            onClick={() => {
-                                setPopupStudentFormStudent(null)
-                                setPopupStudentFormOpen(true)
-                            }}
-                            className='btn-primary mb-20'
-                        >
-                            Add new student
-                        </button>
                     </div>
 
-                    <div className='grid grid-cols-2'>
-                        <Searchbar sortingTextsearch={sortingTextsearch} setSortingTextsearch={setSortingTextsearch}/>
-                    </div>
-
-                    <div className='mt-6'>
-                        <div className='block text-lg leading-6 font-medium text-gray-900'>
-                            Filters
-                            <p className='mt-1 mb-1.5 text-sm text-gray-500'>
-                                Filter by subjects
-                            </p>
-                            <SubjectSelector allPossibleLabels={allSubjects} selectedLabels={selectedSubjects} setSelectedLabels={setSelectedSubjects} />
+                    <div className='grid grid-cols-4'>
+                        <div className='col-span-3'>
+                            <Searchbar sortingTextsearch={sortingTextsearch} setSortingTextsearch={setSortingTextsearch}/>
                         </div>
+                        <div className='col-start-5'>
+                            <button
+                                type='button'
+                                onClick={() => {
+                                    setPopupStudentFormStudent(null)
+                                    setPopupStudentFormOpen(true)
+                                }}
+                                className='btn-primary'
+                            >
+                                Add new student
+                            </button>
+                        </div>
+                    </div>
 
-                        {/*<FilterPopover allCategories={['Maths', 'Science']} currentSort={'Maths'}/>*/}
+                    <div className='mt-7 block leading-6 text-gray-700 mb-3'>
+                        <p className='mb-1'>Filter by subjects</p>
+                        <SubjectSelector allPossibleLabels={allSubjects} selectedLabels={selectedSubjects} setSelectedLabels={setSelectedSubjects} />
                     </div>
 
                     <div className="mt-6">
