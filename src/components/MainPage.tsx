03b1b14684d71fa8aa3a209414e9d54e9bb519f3
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-20 23:13:21 +0000

    added react-hot-toast

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -43,7 +43,7 @@ export default function MainPage({ user }: Props) {
     }
 
     return (
-        <div className="overflow-x-clip overflow-y-clip flex w-screen bg-neutral-800 text-white">
+        <div className="overflow-x-clip flex w-screen bg-neutral-800 text-white">
             <motion.div
                 key="slider-modal"
                 className={`w-screen md:w-[20vw]`}
