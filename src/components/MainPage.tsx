0526c895fe1ca56f0aec197c7d578b2fd7d67687
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-20 21:32:55 +0000

    framer motion animations

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 import {User} from "firebase/auth";
 import ChatScreen from "./open-chat/ChatScreen";
 
+import { motion, AnimatePresence } from "framer-motion"
+
+
 type Props = {
     user: User
 }
@@ -15,32 +18,100 @@ export default function MainPage({ user }: Props) {
     useEffect(() => { if (selectedChatroomId != '') setMobileChatOpen(true) }, [selectedChatroomId])
     useEffect(() => { if (!mobileChatOpen) setSelectedChatroomId('') }, [mobileChatOpen])
 
+
+    const isMobile = window.innerWidth < 768;
+    const initialVariants: any = (fromLeft: boolean) => {
+        // if (!isMobile) return undefined
+        return { x: fromLeft ? -700 : 700 }
+    }
+    const animationVariants: any = (show: boolean, fromLeft: boolean) => {
+        if (!isMobile)
+            return { x: 0, display: 'block' }
+
+        return show ? {
+            x: 0,
+            transition: {duration: 0.3},
+            display: 'block',
+        } : {
+            x: fromLeft ? -700 : 700,
+            transition: {duration: 0.3},
+            position: 'absolute',
+            transitionEnd: {
+                display: "none",
+            }
+        }
+    }
+
     return (
-        <div className="md:overflow-y-hidden flex w-screen h-screen bg-neutral-800 text-white">
-            <div className={`${mobileChatOpen ? 'hidden' : 'block'} w-screen md:w-[20vw] md:block`}>
+        <div className="overflow-x-clip overflow-y-hidden flex w-screen h-screen bg-neutral-800 text-white">
+            <motion.div
+                key="slider-modal"
+                className={`w-screen md:w-[20vw]`}
+                initial={ initialVariants(true) }
+                animate={ animationVariants(!mobileChatOpen, true) }
+                transition={{ type: "just" }}
+            >
                 <Sidebar
                     user={user}
                     selectedChatroomId={selectedChatroomId} setSelectedChatroomId={setSelectedChatroomId}
                     allChatrooms={allChatrooms} setAllChatrooms={setAllChatrooms}
                 />
-            </div>
+            </motion.div>
 
-            { selectedChatroomId != '' ? (
-                <div className={`${mobileChatOpen ? 'block' : 'hidden'} md:block w-screen md:w-[80vw] md:overflow-y-scroll scrollbar md:ml-auto`}>
-                    <ChatScreen
-                        user={user}
-                        selectedChatroomId={selectedChatroomId}
-                        selectedChatroom={allChatrooms.find(c => c.id == selectedChatroomId)}
-                        setMobileChatOpen={setMobileChatOpen}
-                    />
-                </div>
-            ) : (
-                <div className={`${mobileChatOpen ? 'block' : 'hidden'} md:block w-screen md:w-[80vw] m-auto`}>
-                    <h2 className="text-2xl font-semibold text-center">
-                        Click on a chat or create a new chat
-                    </h2>
-                </div>
-            )}
+            <motion.div
+                key="mainchat-modal"
+                className={`w-screen md:w-[80vw]`}
+                initial={ initialVariants(false) }
+                animate={ animationVariants(mobileChatOpen, false) }
+                transition={{ type: "just" }}
+            >
+                { selectedChatroomId != '' ? (
+                    <div className={`w-full md:ml-auto overflow-y-scroll scrollbar `}>
+                        <ChatScreen
+                            user={user}
+                            selectedChatroomId={selectedChatroomId}
+                            selectedChatroom={allChatrooms.find(c => c.id == selectedChatroomId)}
+                            setMobileChatOpen={setMobileChatOpen}
+                        />
+                    </div>
+                ) : (
+                    <div className={`hidden md:block w-full m-auto`}>
+                        <h2 className="text-2xl font-semibold text-center">
+                            Click on a chat or create a new chat
+                        </h2>
+                    </div>
+                )}
+            </motion.div>
         </div>
     )
+
+    // OLD VERSION - NO FRAMER MOTION
+    // return (
+    //     <div className="md:overflow-y-hidden flex w-screen h-screen bg-neutral-800 text-white">
+    //         <div className={`${mobileChatOpen ? 'hidden' : 'block'} w-screen md:w-[20vw] md:block`}>
+    //             <Sidebar
+    //                 user={user}
+    //                 selectedChatroomId={selectedChatroomId} setSelectedChatroomId={setSelectedChatroomId}
+    //                 allChatrooms={allChatrooms} setAllChatrooms={setAllChatrooms}
+    //             />
+    //         </div>
+    //
+    //         { selectedChatroomId != '' ? (
+    //             <div className={`${mobileChatOpen ? 'block' : 'hidden'} md:block w-screen md:w-[80vw] md:overflow-y-scroll scrollbar md:ml-auto`}>
+    //                 <ChatScreen
+    //                     user={user}
+    //                     selectedChatroomId={selectedChatroomId}
+    //                     selectedChatroom={allChatrooms.find(c => c.id == selectedChatroomId)}
+    //                     setMobileChatOpen={setMobileChatOpen}
+    //                 />
+    //             </div>
+    //         ) : (
+    //             <div className={`${mobileChatOpen ? 'block' : 'hidden'} md:block w-screen md:w-[80vw] m-auto`}>
+    //                 <h2 className="text-2xl font-semibold text-center">
+    //                     Click on a chat or create a new chat
+    //                 </h2>
+    //             </div>
+    //         )}
+    //     </div>
+    // )
 }
--- src/utils/time-formatter.ts
+++ src/utils/time-formatter.ts
@@ -1,18 +1,7 @@
 export default function dateFormatter(date: Date) {
     const month = date.toLocaleString("default", { month: "short" });
-    const day = date.toLocaleString("default", { day: "numeric" });
+    const day = date.toLocaleString("default", { day: "2-digit" });
+    const time = date.toLocaleString("default", { timeStyle: 'short' });
 
-    const hour = date.toLocaleString("default", { hour: "2-digit" });
-    const minute = date.toLocaleString("default", { minute: "2-digit" });
-
-    return `${day} ${month}  ${hour}:${minute}`
+    return `${day} ${month}  ${time}`
 }
-
-// export default function dateFormatter(date: Date) {
-//     // Get year, month, and day part from the date
-//     const year = date.toLocaleString("default", { year: "numeric" });
-//     const month = date.toLocaleString("default", { month: "2-digit" });
-//     const day = date.toLocaleString("default", { day: "2-digit" });
-//
-//     return `${year}-${month}-${day}`
-// }
