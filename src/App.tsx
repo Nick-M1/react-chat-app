5779185b2edd21c92a89969f5065853785e96f63
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-24 20:50:23 +0000

    user signin page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ import SigninPage from "./components/authentication/SigninPage";
 export default function App() {
     const [user, userLoading, userError] = useAuthState(auth)
 
-    if (userLoading)
-        return <div>Loading</div>
-    if (user == null)
-        return <SigninPage/>
-
-    return (
-        <MainPage user={user}/>
-    )
+    // if (userLoading)
+        return (
+            <div className='flex flex-col justify-center items-center w-full h-screen-withmobile bg-neutral-900'>
+                <h1 className='text-gray-100 font-semibold text-4xl animate-pulse'>Loading...</h1>
+                <img src='/homepage-gif.gif' alt='' className='md:w-[25vw] h-[25vw] animate-pulse'/>
+            </div>
+        )
+    // if (user == null || typeof userError != 'undefined')
+    //     return <SigninPage/>
+    //
+    // return (
+    //     <MainPage user={user}/>
+    // )
 }
